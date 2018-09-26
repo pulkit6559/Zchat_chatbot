a22f752ab9679946508f5dc18c693c7afd6e6934
pulkit6559 <pulkitarora7.fas@gmail.com>
2018-09-26 23:26:16 +0530

    gradient clipping

--- preprocesssing.py
+++ preprocesssing.py
@@ -301,8 +301,16 @@ training_predictions, test_predictions = seq2seq_model(tf.reverse(inputs, [-1]),
                                                        questionswords2int)
     
   
-
-
+#setting up the Loss Error, optimizer and Gradient clipping
+with tf.name_scope("optimization"):
+    loss_error = tf.contrib.seq2seq.sequence_loss(training_predictions,
+                                                  targets,
+                                                  tf.ones([input_shape[0], sequence_length]))
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    gradients = optimizer.compute_gradients(loss_error)
+    clipped_gradients = [(tf.clip_by_value(grad_tensor, -5.0, 5.0), grad_variable) for grad_tensor, grad_variable in gradients if grad_tensor is not None]
+    optimizer_gradient_clipping = optimizer.apply_gradients(clipped_gradients)
+    
 
 
 
