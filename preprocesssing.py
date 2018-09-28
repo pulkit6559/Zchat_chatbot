c546e39650c079b49d51d05e588d5e39e0803b29
pulkit6559 <pulkitarora7.fas@gmail.com>
2018-09-28 16:45:32 +0530

    bug fixes

--- preprocesssing.py
+++ preprocesssing.py
@@ -167,7 +167,7 @@ def encoder_rnn_layer(rnn_inputs,rnn_size, num_layers, keep_prob, sequence_lengt
     return encoder_state
 
 #Decoding the training set
-def decode_training_set(encoder_state, decoder_cell, decoder_embedded_input, sequence_length):
+def decode_training_set(encoder_state, decoder_cell, decoder_embedded_input, sequence_length, decoding_scope, output_function, keep_prob, batch_size):
     attention_states = tf.zeros([batch_size,1, decoder_cell.output_size])
     attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states,
                                                                                                                                     attention_option='bahdanau',
@@ -189,7 +189,7 @@ def decode_training_set(encoder_state, decoder_cell, decoder_embedded_input, seq
 ######### Decoding the test/validation set
 
 
-def decode_test_set(encoder_state, decoder_cell, decoder_embedded_input, sos_id, eos_id,maximum_length, num_words, sequence_length):
+def decode_test_set(encoder_state, decoder_cell, decoder_embeddings_matrix, sos_id, eos_id,maximum_length, num_words, decoding_scope, output_function, keep_prob, batch_size):
     attention_states = tf.zeros([batch_size,1, decoder_cell.output_size])
     attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states, attention_option='bahdanau',num_units = decoder_cell.output_size)
     test_decoder_function = tf.contrib.seq2seq.attention_decoder_fn_inference(output_function,
@@ -198,13 +198,13 @@ def decode_test_set(encoder_state, decoder_cell, decoder_embedded_input, sos_id,
                                                                               attention_values,
                                                                               attention_score_function,
                                                                               attention_construct_function,
-                                                                              decoder_embedded_input,
+                                                                              decoder_embeddings_matrix,
                                                                               sos_id,
                                                                               eos_id,
                                                                               maximum_length,
                                                                               num_words,
                                                                               name = "attn_dec_inf")
-    test_predictions, decoder_final_state, decoder_final_context_state =  tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell, test_decoder_function)
+    test_predictions, decoder_final_state, decoder_final_context_state =  tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell, test_decoder_function, scope = decoding_scope)
     return test_predictions
 
 def decoder_rnn(decoder_embedded_input, decoder_embeddings_matrix, encoder_state, num_words, sequence_length, rnn_size, num_layers, word2int, keep_prob, batch_size):
@@ -218,15 +218,33 @@ def decoder_rnn(decoder_embedded_input, decoder_embeddings_matrix, encoder_state
                                                                       num_words,
                                                                       None,
                                                                       scope = decoding_scope,
-                                                                      weights_intializer = weights,
+                                                                      weights_initializer = weights,
                                                                       biases_initializer = biases)
-        training_predictions = decode_training_set(encoder_state,
-                                                   decoder_cell,
-                                                   decoder_embedded_input,
-                                                   sequence_length,
-                                                   )
+        training_predictions = decode_training_set(encoder_state, decoder_cell,decoder_embedded_input, sequence_length, decoding_scope,output_function,keep_prob,batch_size)
         decoding_scope.reuse_variables()
-        test_predictions = decode_test_set()
+        test_predictions = decode_test_set(encoder_state,
+                                           decoder_cell,
+                                           decoder_embeddings_matrix,
+                                           word2int['<SOS>'],
+                                           word2int['<EOS>'],
+                                           sequence_length - 1,
+                                           num_words,
+                                           decoding_scope,
+                                           output_function,
+                                           keep_prob,
+                                           batch_size)
+        decoding_scope.reuse_variables()
+        test_predictions = decode_test_set(encoder_state,
+                                           decoder_cell,
+                                           decoder_embeddings_matrix,
+                                           word2int['<SOS>'],
+                                           word2int['<EOS>'],
+                                           sequence_length - 1,
+                                           num_words,
+                                           decoding_scope,
+                                           output_function,
+                                           keep_prob,
+                                           batch_size)
         
 
 # Building Seq2seq model
