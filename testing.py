0bdd03680569fc22ed104dd82e428b96d9200b85
pulkit6559 <pulkitarora7.fas@gmail.com>
2018-10-06 13:48:24 +0530

    quest2int

--- testing.py
+++ testing.py
@@ -4,3 +4,7 @@ session.run(tf.global_variables_initialiser())
 saver = tf.train.Saver()
 
 saver.restore(session, checkpoint)
+
+def convert_string2int(question, word2int):
+    question = clean_text(questions)
+    return [word2int.get(word, word2int['<OUT']) for word in questions.split()]
