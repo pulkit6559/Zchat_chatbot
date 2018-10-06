9f2c07c7a28e4d8112d0bb5627241c786cbbbcd2
pulkit6559 <pulkitarora7.fas@gmail.com>
2018-10-06 13:52:24 +0530

    .

--- testing.py
+++ testing.py
@@ -8,3 +8,11 @@ saver.restore(session, checkpoint)
 def convert_string2int(question, word2int):
     question = clean_text(questions)
     return [word2int.get(word, word2int['<OUT']) for word in questions.split()]
+
+while(True):
+    question = input("you: ")
+    if(question == "Googbye"):
+        break
+    question = convert_string2int(question, questionswords2int)
+    questions = question + [questionswords2int['<PAD>']]* (20-len(questions))
+    question = question + 
